eecada090d4ea4f0232ab94e117f92c576156252
Josh Mitchell <jmitch13@csu.fullerton.edu>
2024-01-28 22:54:42 -0800

    Add pocast files

---
+++ controllers/podcast_controller.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const path = require("path");
+const bodyParser = require("body-parser");
+const { data } = require("jquery");
+
+//Try middleware
+
+var urlendcodedParser = bodyParser.urlencoded({extended: false});
+
+module.exports = function(app){
+
+    app.get('/reading', function(req, res){
+        fetch(url).then((res) => {
+            res.json().then((res1) => {
+                console.log(res1);
+            })
+        })
+        let url = "https://api.spotify.com/v1/shows/3uLQxrMZ9xsqj7FcwNaWrR/episodes"
+    });
+}
